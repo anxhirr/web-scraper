1183201a1d41312e628f22393802479eadf5cdd5
anxhirr <anxhirr@gmail.com>
2023-03-06 20:21:25 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline'
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <DocumentMagnifyingGlassIcon className='h-64 w-64 text-indigo-600/20' />
+        <h1 className='text-3xl mt-2 font-bold mb-5 text-center'>
+          Wellcome to Web Scraper
+        </h1>
+      </div>
+    </div>
+  )
+}
+export default HomePage
